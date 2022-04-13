b1790853d07ac8f3797de46b2215492a2277ef6b
botoroboto <>
2022-04-13 20:55:31 -0300

    Agrega version mejorada de cliqueMasInfluyente - Refactor de código

--- clique.cpp
+++ clique.cpp
@@ -7,36 +7,36 @@ clique::clique() {
   this->actores = {};
 }
 
-vector<actor> clique::getActores(){
-  return this->actores; // expression must have class type but it has type "clique *"
+vector<actor> clique::getActores() {
+  return this->actores;
 }
 
 clique clique::clone() {
-    clique newClique = *new clique();
-    int a = this->influencia;
-    newClique.setInfluencia(a);
-    for (int i = 0; i < this->actores.size(); ++i) {
-        actor b = this->actores[i];
-        newClique.actores.push_back(b);
-    }
+  clique newClique = *new clique();
+  int a = this->influencia;
+  newClique.setInfluencia(a);
+  for (int i = 0; i < this->actores.size(); ++i) {
+    actor b = this->actores[i];
+    newClique.actores.push_back(b);
+  }
 
-    return newClique;
+  return newClique;
 }
 
 // void clique::popActor() {
 //   this->actores.pop_back();
 // }
 
-clique clique::addActor(actor newActor){
+clique clique::addActor(actor newActor) {
   this->actores.push_back(newActor);
   return *this;
 }
 
-int clique::setInfluencia(int newInfluencia){
+int clique::setInfluencia(int newInfluencia) {
   this->influencia = newInfluencia;
   return this->influencia;
 }
 
-int clique::getInfluencia(){
+int clique::getInfluencia() {
   return this->influencia;
 }
--- main.cpp
+++ main.cpp
@@ -17,36 +17,38 @@ string ID_AMISTAD = "e";
 vector<actor> Actores;
 vector<vector<bool>> Amistades;
 
-bool sonAmigos(actor a1, actor a2){
+bool sonAmigos(actor a1, actor a2) {
   return Amistades[a1.id][a2.id];
 };
 
-int influenciaMax = 0; //holder para la influencia Máxima voy a ir actualizando con la mejor que tenga hasta el momento
+int influenciaMax = 0; // Counter para la influencia máxima, voy a ir actualizando con la mejor que tenga hasta el momento
 
+// TODO - Como podemos refactorizar esto para sacarlo del main? O deberiamos tener 4 main.cpp?
 clique cliqueMasInfluyenteBT(clique cliqueActual, int actorActual, int influenciaParcial, int sumaRestante) {
-  //Caso base
-  if(actorActual > Actores.size() - 1){
+  // Caso base
+  if (actorActual > Actores.size() - 1) {
     cliqueActual.setInfluencia(influenciaParcial);
-      if(influenciaMax < influenciaParcial){
+      if (influenciaMax < influenciaParcial) {
       influenciaMax = influenciaParcial;
     }
     return cliqueActual;
   }
-  
-  //Poda de optimalidad
-  if(sumaRestante + influenciaParcial < influenciaMax){
-    return cliqueActual; //No sigo recorriendo nada el arbol porque la suma ya no puede superar al max actual
+
+  // Poda de optimalidad
+  if (sumaRestante + influenciaParcial < influenciaMax) {
+    return cliqueActual; // No sigo recorriendo nada el arbol porque la suma ya no puede superar al max actual
   }
 
-  //Poda de factibilidad
+  // Poda de factibilidad
   bool hayAmistad = true;
-  for(actor a : cliqueActual.getActores()){
-    if(!sonAmigos(a, Actores[actorActual])){
+  for (actor a : cliqueActual.getActores()) {
+    if (!sonAmigos(a, Actores[actorActual])) {
       hayAmistad = false;
       break;
     }
   }
-  if(!hayAmistad){
+
+  if (!hayAmistad) {
     return cliqueMasInfluyenteBT(
       cliqueActual,
       actorActual + 1,
@@ -55,63 +57,95 @@ clique cliqueMasInfluyenteBT(clique cliqueActual, int actorActual, int influenci
     );
   }
 
-  clique loAgrego = cliqueMasInfluyenteBT( //agrego el actor actual al clique (tengo cierta intuición de que si corro esta rama del arbol primero va a ser mas eficiente la poda de optimalidad cuando corra la otra rama)
+  // Agrego el actor actual al clique (tengo cierta intuición de que si corro esta rama del arbol primero va a ser mas eficiente la poda de optimalidad cuando corra la otra rama)
+  clique loAgrego = cliqueMasInfluyenteBT(
     cliqueActual.clone().addActor(Actores[actorActual]),
     actorActual + 1,
     influenciaParcial + Actores[actorActual].influencia,
     sumaRestante - Actores[actorActual].influencia
   );
-  clique noLoAgrego = cliqueMasInfluyenteBT( //no agrego el actor actual al clique
+
+  // No agrego el actor actual al clique
+  clique noLoAgrego = cliqueMasInfluyenteBT(
     cliqueActual,
     actorActual + 1,
     influenciaParcial,
     sumaRestante - Actores[actorActual].influencia
   );
 
-  return noLoAgrego.getInfluencia() > loAgrego.getInfluencia() ? noLoAgrego : loAgrego; //Devuelvo el clique con mas influencia
+  // Devuelvo la clique con mas influencia
+  return noLoAgrego.getInfluencia() > loAgrego.getInfluencia() ? noLoAgrego : loAgrego;
 };
 
-cliqueMasInfluyenteBT2(clique cliqueActual, vector<actor> actoresRestantes, int influenciaParcial, int sumaRestante){
-  //caso base
-  if(actoresRestantes.size() < 1){
+clique cliqueMasInfluyenteBT2(clique cliqueActual, vector<actor> actoresRestantes, int influenciaParcial, int sumaRestante){
+  // caso base
+  if (actoresRestantes.size() < 1) {
     cliqueActual.setInfluencia(influenciaParcial);
     if(influenciaMax < influenciaParcial){
       influenciaMax = influenciaParcial;
     }
     return cliqueActual;
   }
-  
+ 
   //Poda de optimalidad
   if(sumaRestante + influenciaParcial < influenciaMax){
     return cliqueActual; //No sigo recorriendo nada el arbol porque la suma ya no puede superar al max actual
   }
+
+  bool sonTodosAmigos = false;
+
+  for (actor a : actoresRestantes) {
+    for (actor a2 : actoresRestantes) {
+      if (a.id < a2.id) {
+        if (sonAmigos(a, a2)) {
+          sonTodosAmigos = true;
+        } else {
+          sonTodosAmigos = false;
+          break;
+        }
+      }
+    }
+    if (!sonTodosAmigos) {
+      break;
+    }
+  }
+
+  if (sonTodosAmigos) { // esto significa que todos son amigos entre todos de K por lo que podemos meterlos a todos al clique
+    cliqueActual.setInfluencia(sumaRestante + influenciaParcial); // la influencia es toda la que quedaba mas la que ya traía
+    for(actor a : actoresRestantes){
+      cliqueActual.addActor(a);
+    }
+    if (sumaRestante + influenciaParcial > influenciaMax) {
+      influenciaMax = sumaRestante + influenciaParcial;
+    }
+    return cliqueActual;
+  }
   
-  actor actorActual = actoresRestantes.pop();
-  
+  actor actorActual;
+  actorActual.id = actoresRestantes[0].id;
+  actorActual.influencia = actoresRestantes[0].influencia;
+  actoresRestantes.erase(actoresRestantes.begin());
   
   vector<actor> nuevoVectorDeActoresRestantes = {};
   int nuevaSumaRestante = sumaRestante;
-  for(actor a : actoresRestantes ){
-    if(sonAmigos(a, actorActual){
-      nuevoVectorDeActoresRestantes.push_back(a);
-    }else{
-      nuevaSumaRestante - a.influencia;
+  for (actor a : actoresRestantes) {
+    if (sonAmigos(a, actorActual)) {
+      actor actorTemporal;
+      actorTemporal.id = a.id;
+      actorTemporal.influencia = a.influencia;
+      nuevoVectorDeActoresRestantes.push_back(actorTemporal);
+    } else {
+      nuevaSumaRestante -= a.influencia;
     }
   }
 
-  if(nuevaSumaRestante + influenciaParcial < influenciaMax){//la suma restante todvía tiene al actor actual en cuenta...
-    return cliqueActual; //No sigo recorriendo nada el arbol porque la suma ya no puede superar al max actual
-  }
-
-  if(nuevoVectorDeActoresRestantes.size() == actoresRestantes.size()){//esto significa que todos son amigos entre todos de K por lo que podemos meterlos a todos al clique
-    cliqueActual.setInfluencia(sumaRestante + influenciaParcial); //la influencia es toda la que quedaba mas la que ya traía
-    for(actor a : actoresRestantes){
-      clique.actores.addActor(a);
-    }
-    if(sumaRestante + influenciaParcial > influenciaMax){
-      influenciaMax = sumaRestante + influenciaParcial
-    }
-    return cliqueActual;
+  if (nuevaSumaRestante + influenciaParcial < influenciaMax) { // la suma restante todavía tiene al actor actual en cuenta...
+    return cliqueMasInfluyenteBT2( // no agrego el actor actual al clique
+      cliqueActual,
+      actoresRestantes,
+      influenciaParcial,
+      sumaRestante - actorActual.influencia
+    ); // No sigo recorriendo nada el arbol porque la suma ya no puede superar al max actual
   }
 
   nuevaSumaRestante -= actorActual.influencia; //le resto a la suma restante la influencia del actor actual ya que ahora voy a agregarlo o descartarlo
@@ -120,22 +154,23 @@ cliqueMasInfluyenteBT2(clique cliqueActual, vector<actor> actoresRestantes, int 
     cliqueActual.clone().addActor(actorActual),
     nuevoVectorDeActoresRestantes,
     influenciaParcial + actorActual.influencia,
-    nuevaSumaRestante,
+    nuevaSumaRestante
   );
+
   clique noLoAgrego = cliqueMasInfluyenteBT2( //no agrego el actor actual al clique
     cliqueActual,
     actoresRestantes,
     influenciaParcial,
-    nuevaSumaRestante
+    sumaRestante - actorActual.influencia
   );
 
   return noLoAgrego.getInfluencia() > loAgrego.getInfluencia() ? noLoAgrego : loAgrego; //Devuelvo el clique con mas influencia
 }
 
-int main(int argc, char *argv[]) {
-  //Funcion BT Ej 1
-  
-  int sumaInfluenciaTotal = 0; //Sumatoria de todas las influencias de todos los actores
+// TODO - Parsear desde un file
+int main(long argc, char *argv[]) {
+  // Funcion BT Ej 1
+  int sumaInfluenciaTotal = 0; // Sumatoria de todas las influencias de todos los actores
 
   if (argc < 5) {
     cerr << "Parámetros faltantes: " << endl << "Cantidad de actores y amistades" << endl;
@@ -148,37 +183,37 @@ int main(int argc, char *argv[]) {
     cout << "Los actores no pueden ser 0" << endl; 
     return 1;
   }
-  Amistades = vector< vector<bool> >(N + 1, vector<bool>(N + 1, false));;
+
+  Amistades = vector<vector<bool>>(N + 1, vector<bool>(N + 1, false));
 
   for (int i = M_INDEX + 1; i < argc; i += 3) {
     if (argv[i] == ID_ACTOR) {
-      cout << "Actor: " << stoi(argv[i+1]) << " - Influencia: " << stoi(argv[i+2]) << endl;
+      // Descomentar si se necesita debuggear
+      // cout << "Actor: " << stoi(argv[i+1]) << " - Influencia: " << stoi(argv[i+2]) << endl;
       actor newActor;
       newActor.id = stoi(argv[i+1]);
       newActor.influencia = stoi(argv[i+2]);
-      Amistades[stoi(argv[i+1])][stoi(argv[i+1])] = true;
-        // TODO - Podríamos sortear acá
+      // TODO - Podríamos sortear acá
       Actores.push_back(newActor);
+      sumaInfluenciaTotal += newActor.influencia;
     } else if (argv[i] == ID_AMISTAD) {
-      cout << "Actor " << stoi(argv[i+1]) << " es amigo de Actor " << stoi(argv[i+2]) << endl;
+      // Descomentar si se necesita debuggear
+      // cout << "Actor " << stoi(argv[i+1]) << " es amigo de Actor " << stoi(argv[i+2]) << endl;
       Amistades[stoi(argv[i+1])][stoi(argv[i+2])] = true;
       Amistades[stoi(argv[i+2])][stoi(argv[i+1])] = true;
     }
   };
 
-  for(actor a : Actores){
-      cout << "actor " << a.id << endl;
-      sumaInfluenciaTotal += a.influencia;
+  auto start = chrono::steady_clock::now(); // Empieza el clock
+  clique res = cliqueMasInfluyenteBT2(*new clique(), Actores, 0, sumaInfluenciaTotal);
+  auto end = chrono::steady_clock::now(); // Termina el clock
+  double total_time = chrono::duration<double, milli>(end - start).count();
+  clog << total_time << " ms" << endl;
 
-      for (int i = 1; i < Amistades.size(); ++i) {
-          cout << "amigo de " << i << ' ' << Amistades[i][a.id] << endl;
-      }
+  cout << res.getInfluencia() << endl;
+  for (actor a : res.getActores()) {
+    cout << a.id << " ";
   }
-  clique s = *new clique();
-  clique res = cliqueMasInfluyenteBT(s, 0, 0, sumaInfluenciaTotal);
-  cout << "Influencia " << res.getInfluencia() << endl;
-    for (actor a : res.getActores()) {
-        cout << "Actor: " << a.id << endl;
-    }
+  cout << endl;
   return 0;
 }
