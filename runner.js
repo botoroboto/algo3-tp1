b407f7ea50a595c748f2df0acd355166531c3df9
botoroboto <>
2022-04-14 12:21:48 -0300

    Agrega runner en Javascript para correr instancias - Agrega sorting a los actores, segun influencia

---
+++ runner.js
@@ -0,0 +1,25 @@
+const { execFileSync, execSync } = require('child_process');
+const fs = require('fs');
+
+const filePath = './build/Release/algo3-tp1.exe';
+
+// Cambiar esto para usar otras instancias
+const basePathInstancias = './instancias/test';
+let printTimings = '';
+
+fs.readdirSync(basePathInstancias).forEach(instancia => {
+  const stdout = execFileSync(filePath, [`${basePathInstancias}/${instancia}`, 'desc', 'debug'], { encoding: 'utf-8' });
+  if (stdout) {
+    console.log(stdout);
+    const executionTime = stdout.split('Tiempo de ejecucion: ');
+    const timing = executionTime[1].split(' ms')[0].trim();
+    const influencia = executionTime[0].split("Influencia: ")[1].trim();
+    const newLine = `${instancia}: ${influencia}, ${timing}ms\n`;
+    printTimings = `${printTimings}${newLine}`;
+  };
+});
+
+console.log(printTimings);
+fs.writeFileSync('timings.txt', printTimings, { flag: 'w' }, err => {
+  console.log("Hubo un error al escribir timings.txt, ", err);
+});
