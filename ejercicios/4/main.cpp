550604601b90004711b9348c97cf8b3059eebc14
azzollinimauro <mauro.azzo3210@gmail.com>
2022-04-20 01:35:12 -0300

    Ejercicio 4

--- ejercicios/4/main.cpp
+++ ejercicios/4/main.cpp
@@ -7,60 +7,73 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
-#include "./radix_sort.h"
-
+#include "../../common/clique.h"
 using namespace std;
 
 int FILE_INDEX = 1;
 int SORT_INDEX = 2;
 int DEBUG_INDEX = 3;
 int N;
-vector<int> S = {};  // tiempo inicio
-vector<int> T = {};  // tiempo fin
-vector<int> B = {};  //beneficio
 const int UNDEFINED_VALUE = -1;
+vector<int> actividadSiguiente;
+vector<int> resGreedy;
 
-
-//TODO levantar del archivo
-vector<pair<int, int>> instancia_1 = {
-        make_pair(4, 5),
-        make_pair(3, 7),
-        make_pair(0, 3),
-        make_pair(2, 8),
-        make_pair(6, 10)
+struct actividad {
+    int inicio;
+    int fin;
 };
-
-vector<pair<int, int>> instancia_2 = {
-        make_pair(6, 10),
-        make_pair(1, 5),
-        make_pair(8, 10),
-        make_pair(7, 9),
-        make_pair(2, 5),
-        make_pair(3, 4),
-        make_pair(5, 9)
+struct actividadConIndice{
+    actividad tarea;
+    int index;
 };
+struct resultado {
+    int res = -1;
+    vector<actividad> actividades = {};
+};
+resultado undefinedValue;
 
-void beneficio_actividades(vector<pair<actividad, int>>& actividades, pair<int, vector<int>>& res){
-    int indice = 0;
-    res.first = 1;
-    res.second.push_back(actividades[0].second);
+vector<actividadConIndice> actividades = {};
+vector<actividadConIndice> ordenadoPorInicio = {};
+vector<actividadConIndice> ordenadoPorFin= {};
 
-    for (int i = 1; i < actividades.size(); ++i) {
-        //compara el inicio de la actividad que esta siendo iterada con el final de la ultima actividad agregada
-        if(actividades[i].first.s > actividades[indice].first.t){
-            res.first++;
-            res.second.push_back(actividades[i].second);
-            indice = i;
+vector<int> generarVectorDeSiguienteActividad(){
+    vector<int> res(N , N);
+    int inicio = 0;
+    int fin = 0;
+    while(inicio<N || fin<N){
+        if(inicio==N){
+            while(fin<N) {
+                res[ordenadoPorFin[fin].index] = N;
+                fin++;
+            }
+        }
+        else if(ordenadoPorInicio[inicio].tarea.inicio <= ordenadoPorFin[fin].tarea.fin){
+            inicio++;
+        }else{
+            res[ordenadoPorFin[fin].index] = inicio;
+            fin++;
         }
     }
-    //return res;
+    return res;
 }
-/*TODO
- * IDEA de implementacion para radix_sort_modificado (sin terminar aun)
- * Usar un algoritmo de sorting tipo radix-sort con bucket-sort en la entrada ordenando por los horarios de finalizacion de menor a mayor:
-    - Al recorrer el arreglo y separar en buckets, guardar el indice de la posicion original que tenía cada elemento en el arreglo original
-    - Ordenar de esta forma costaría O(n) ya que se recorre una unica vez el arreglo.
- */
+
+vector<int> greedy() {
+    vector<int> res = {};
+    int k=0;
+    res.push_back(ordenadoPorFin[k].index);
+    for (int j = 1; j <= N ; ++j) {
+        if(ordenadoPorFin[j].tarea.inicio > ordenadoPorFin[k].tarea.fin) {
+            res.push_back(ordenadoPorFin[j].index);
+            k = j;
+        }
+
+    }
+
+    return res;
+}
+
+
+// TODO - Parsear desde un file
 int main(int argc, char *argv[]) {
 
     ios::sync_with_stdio(false);
@@ -80,45 +93,67 @@ int main(int argc, char *argv[]) {
             N = stoi(linea_instancia);
             int number;
             cout << "n:" << N << endl;
-            B.resize(N);
-            S.resize(N);
-            T.resize(N);
             int i = 1;
+
+            actividadConIndice actividadDummy;
             while (archivo_instancia >> number) {
-                if (i % 3 == 0) {
-                    B[(i/3) - 1] = number;
-                } else if (i % 3 == 2) {
-                    T[i/3] = number;
-                }else {
-                    S[i/3] = number;
+                if (i % 2 == 0) {
+                    actividadDummy.tarea.fin = number;
+                    actividadDummy.index = (i / 2) - 1;
+                    actividades.push_back(actividadDummy);
+                } else {
+                    actividadDummy.tarea.inicio = number;
                 }
                 i++;
             }
             archivo_instancia.close();
-        }}
+        }
+    }
+    vector<vector<actividadConIndice>> bucketsFin(2 * N + 1, vector<actividadConIndice>{});
+    vector<vector<actividadConIndice>> bucketsInicio(2 * N + 1, vector<actividadConIndice>{});
 
+
+    for (int a = 0; a < actividades.size(); a++) {
+        actividadConIndice dummy;
+        dummy.tarea = actividades[a].tarea;
+        dummy.index = actividades[a].index;
+        bucketsFin[actividades[a].tarea.fin].push_back(dummy);
+        bucketsInicio[actividades[a].tarea.inicio].push_back(dummy);
+    }
+    for (vector<actividadConIndice> bucket : bucketsFin) {
+        for (actividadConIndice a : bucket) {
+            ordenadoPorFin.push_back(a);
+        }
+    }
+    for (vector<actividadConIndice> bucket : bucketsInicio) {
+        for (actividadConIndice a : bucket) {
+            ordenadoPorInicio.push_back(a);
+        }
+    }
     if (N == 0) {
-        cout << "Los actores no pueden ser 0" << endl;
+        cout << "Las actividades no pueden ser 0" << endl;
         return 1;
     }
 
-    pair<int, vector<int>> soluciones_1;
-    vector<pair<actividad, int>> actividades_1 = radix_sort_modificado(instancia_1);
-    beneficio_actividades(actividades_1, soluciones_1);
+    auto start = chrono::steady_clock::now(); // Empieza el clock
+    auto end = chrono::steady_clock::now(); // Termina el clock
+    double total_time = chrono::duration<double, milli>(end - start).count();
+    vector<vector<resultado>> M(N + 1, vector<resultado>(2 * N + 1, undefinedValue));
+    actividadSiguiente.resize(2 * N, UNDEFINED_VALUE);
 
-    cout << soluciones_1.first << endl;
-    for (int posicion : soluciones_1.second) {
-        cout << posicion << ", ";
+    actividadSiguiente = generarVectorDeSiguienteActividad();
+    resGreedy = greedy();
+
+    cout << resGreedy.size() <<endl;
+
+    for (int i = 0; i < resGreedy.size(); ++i) {
+        cout << resGreedy[i] << "  ";
     }
 
-    pair<int, vector<int>> soluciones_2;
-    vector<pair<actividad, int>> actividades_2 = radix_sort_modificado(instancia_2);
-    beneficio_actividades(actividades_2, soluciones_2);
+    cout << endl;
 
-    cout << soluciones_2.first << endl;
-    for (int posicion : soluciones_2.second) {
-        cout << posicion << ", ";
-    }
+    cout << "Tiempo de ejecucion: " << total_time << " ms" << endl;
 
+    cout << endl;
     return 0;
 }
